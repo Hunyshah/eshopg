b6593b94d95a6f79bb9f7be6c43a89fead25a3d0
hunyshah <hunyshah0342@gmail.com>
2023-05-14 08:06:45 -0700

    makeProvider

---
+++ src/components/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const MainSection = () => {
+  return (
+    // es main next component ho ga item list ho ge sath form ho ga
+    <div>MainSection</div>
+  );
+};
+
+export default MainSection;
